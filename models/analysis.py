47c8cb975e751181cd0a1bb17671bb3d2079dbc1
meyyappan055 <meyyappan055@gmail.com>
2024-10-12 21:43:49 +0530

    feat: Implement moving average crossover strategy and testing the data

---
+++ models/analysis.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+import pandas as pd
+from data import engine
+
+def fetch_datetime():
+    date_time = pd.read_sql("SELECT datetime FROM trade_data" ,con=engine)
+    return date_time
+
+def fetch_close():
+    close = pd.read_sql("SELECT close FROM trade_data" ,con=engine)
+    return close
+
+def calculate_moving_averages():
+    df = fetch_close()
+    df['short_mavg'] = df['close'].rolling(window=30, min_periods=1).mean()
+    df['long_mavg'] = df['close'].rolling(window=120, min_periods=1).mean()
+    return df
+
+def generate_signals(df):
+    df['signal'] = 0
+    df.loc[df['short_mavg'] > df['long_mavg'], 'signal'] = 1 #buy = 1
+    df.loc[df['short_mavg'] < df['long_mavg'], 'signal'] = -1 #sell = -1
+    df['short_mavg'] = df['short_mavg'].round(2)
+    df['long_mavg'] = df['long_mavg'].round(2)
+    # pd.set_option('display.max_rows', None)
+    # pd.set_option('display.max_columns', None)
+    return df
+
+df = generate_signals(calculate_moving_averages())
---
+++ models/testing.py
@@ -0,0 +1,20 @@
+from analysis import df
+
+def testing_strategy(df,capital):
+    shares = 0
+    df['portfolio'] = 0
+    for i in range(len(df)):
+        if df['signal'].iloc[i] == 1:
+            if shares == 0:
+                shares = capital/df['close'].iloc[i]
+                capital = 0
+        elif df['signal'].iloc[i] == -1:
+            if shares > 0:
+                capital = shares*df['close'].iloc[i]
+                shares = 0
+        df.at[i,'portfolio'] = shares*df['close'].iloc[i] + capital
+
+    df['portfolio'] = df['portfolio'].astype(float).round(2)
+    return df
+    
+print(testing_strategy(df,1000)) #testing with a capital of 1000
